88c4ce4670212ccc186071e577cc79db4f67a5aa
leonawicz <matt.rstat@gmail.com>
2017-11-03 16:51:42 -0600

    Load fonts on package load for Windows.

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,4 @@
 .onLoad <- function(libname, pkgname){
   suppressMessages(extrafont::font_import(pattern = "impact", prompt = FALSE))
+  if(.Platform$OS.type == "windows") extrafont::loadfonts("win", quiet = TRUE)
 }
