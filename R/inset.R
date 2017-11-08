af9ef0f5e71a2a984d224d480b2a5a38deeeec00
leonawicz <matt.rstat@gmail.com>
2017-11-08 15:08:19 -0700

    hjust only applied to plot title.

--- R/inset.R
+++ R/inset.R
@@ -125,14 +125,14 @@ memetheme <- function(base_size = 14, base_family = "", base_col = "white"){
   ggplot2::theme_gray(base_size = base_size, base_family = base_family) +
   ggplot2::theme(
     panel.grid.major = ggplot2::element_line(size = 0.5, colour = "gray"),
-    title = ggplot2::element_text(colour = base_col, hjust = 0),
+    title = ggplot2::element_text(colour = base_col),
     axis.text = ggplot2::element_text(colour = base_col),
     axis.ticks = ggplot2::element_line(colour = base_col),
     axis.line = ggplot2::element_line(size = 1, colour = base_col),
     axis.ticks.length = ggplot2::unit(0.35, "cm"), legend.position = "bottom",
     legend.justification = "right", legend.title = ggplot2::element_blank(),
     legend.text = ggplot2::element_text(size = base_size), text = ggplot2::element_text(size = 18),
-    plot.title = ggplot2::element_text(size = 20),
+    plot.title = ggplot2::element_text(size = 20, hjust = 0),
     panel.spacing.x = ggplot2::unit(0.25, "cm"),
     plot.margin = ggplot2::unit(c(0.25, 0.5, 0.25, 0.25), "cm"),
     strip.background = ggplot2::element_rect(fill = "#33333350", colour = base_col),
